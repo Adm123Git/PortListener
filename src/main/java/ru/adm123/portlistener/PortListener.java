b328da00ddc2843640c6bda46ca3bb08cf473e9f
Adm123 <adm123@mail.ru>
2021-02-27 15:03:38 +0300

    commit

---
+++ src/main/java/ru/adm123/portlistener/PortListener.java
@@ -0,0 +1,30 @@
+package ru.adm123.portlistener;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * Просто сервлет, ловящий запросы на порт 8080 и выводящий пойманное в консоль
+ *
+ * @author Dmitry Ushakov at 21.02.21
+ *
+ */
+@WebServlet("")
+public class PortListener extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println(request);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        System.out.println(request.getReader().lines().collect(Collectors.joining(System.lineSeparator())));
+    }
+
+}
